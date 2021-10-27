52f3160e8bd870bcebfa743509fba63b9f03a739
maff1 <49032010+maff1@users.noreply.github.com>
2021-10-27 21:58:07 +0100

    Add files via upload

---
+++ f_dep_cbz.R
@@ -0,0 +1,247 @@
+################################################################################
+# CBZ preprocess and run LIMMA ## v001 ## MAFF #################################
+
+rm(list = ls())
+
+library(data.table)
+library(limma)
+library(Biobase)
+library(missRanger)
+# ---------------------------------------------------------------------------- #
+
+f_clean_dep <-  function(pathDat, impute = TRUE, method = "addZero") {
+  
+dfNames <- c("uniprot", "symbol",
+  toupper(make.names(
+  data.table::fread(file = pathDat, 
+                             sep = "\t", 
+                             select = c(10:29), 
+                             dec = ",", 
+                             header = FALSE, 
+                             skip = 3,
+                             nrows = 1,
+                             data.table = FALSE)
+    )
+  )
+)
+
+df <- data.table::fread(file = pathDat, 
+                            sep = "\t", 
+                            select = c(5:6, 10:29), 
+                            dec = ",", 
+                            header = FALSE, 
+                            skip = 4,
+                            na.strings = c("NaN"),
+                            data.table = FALSE)
+
+df <- as.data.frame(
+  cbind(df[, 1:2],
+        apply(df[, 3:ncol(df)], 
+              2, 
+              as.numeric)
+        )
+)
+
+df$V5 <- trimws(gsub(" .*|-DECOY", "", df$V5))
+df$V6 <- trimws(df$V6)
+df$V6 <- ifelse(df$V6 == "", df$V5, df$V6)
+colnames(df) <- dfNames
+
+if(anyDuplicated(df$uniprot) > 0) {
+  df <- df[-as.numeric(rownames(df[duplicated(df$uniprot),])),]
+  } else {df <- df}
+
+# DROP TWO SAMPLES
+df <- df[, -c(9,12)]
+
+
+metaDat <- df[, 1:2]
+exprDat <- df[, 3:ncol(df)]
+
+expr <- as.data.frame(t(
+  data.frame(
+  exprDat,
+  row.names = metaDat$uniprot
+  )
+))
+
+colsNA = data.frame(percNA = colSums(is.na(expr)/nrow(expr)*100))
+print(paste0("Excluded ", length(rownames(subset(colsNA, percNA == 100))),
+             " proteins with all zero........................................."))
+`%nin%` <- Negate(`%in%`)
+expr <- expr[, colnames(expr) %nin% rownames(subset(colsNA, percNA == 100))]
+metaDat <- metaDat[metaDat$uniprot %nin% rownames(subset(colsNA, percNA == 100)),]
+saveRDS(metaDat, "./RESULTS/metaData_cbz.rds")
+
+# IMPUTATION ----------------------------------------------------------------- #
+if(impute == TRUE & method == "addZero") {
+  expr[is.na(expr)] <- 0
+    } else if(impute == TRUE & method == "randomForest") {
+    expr <- data.frame(
+      missRanger::missRanger(
+      data = expr, formula = .~., seed = 2021, num.trees = 10
+    ), row.names = rownames(expr)
+  )
+  } else if(impute == FALSE & method == "none") {
+    expr
+  }
+
+saveRDS(expr, paste0("./RESULTS/", make.names(pathDat), ".rds"))
+
+# REFORMAT TO ACCOMMODATE FOR EXPRESSION SET OBJECT -------------------------- #
+samples <- data.frame(samples = rownames(expr), 
+                      groups = gsub("\\.[0-9]$|\\.[0-9][A-Z]$|(?<=DMSO)[0-9]", "", 
+                                    rownames(expr), perl = TRUE),
+                      row.names = rownames(expr)
+)
+
+toMatch = data.frame(
+    uniprot = colnames(expr),
+    row.names = colnames(expr)
+)
+
+anno = merge(toMatch, metaDat, by = "uniprot", all.x = TRUE, sort = FALSE)
+anno = data.frame(
+  uniprot = anno$uniprot,
+  symbol = anno$symbol,
+  row.names = anno$uniprot
+)
+  
+phenoData <- new("AnnotatedDataFrame", data = samples)
+featureData <- new("AnnotatedDataFrame", data = anno)
+eset <- ExpressionSet(
+  assayData = as.matrix(t(expr)), 
+  phenoData = phenoData,
+  featureData = featureData
+)
+
+# quantile normalization ----------------------------------------------------- #
+quantile_normalisation <- function(df){
+  df_rank <- apply(df,2,rank,ties.method="min")
+  df_sorted <- data.frame(apply(df, 2, sort))
+  df_mean <- apply(df_sorted, 1, mean)
+  
+  index_to_mean <- function(my_index, my_mean){
+    return(my_mean[my_index])
+  }
+  
+  df_final <- as.data.frame(apply(df_rank, 2, index_to_mean, my_mean=df_mean))
+  rownames(df_final) <- rownames(df)
+  return(df_final)
+}
+
+neset <- ExpressionSet(
+  assayData = as.matrix(
+    log2(quantile_normalisation(exprs(eset)) + 1)), 
+  phenoData = phenoData,
+  featureData = featureData
+)
+
+pdf(file = "./RESULTS/cbz_quantile.normalisation.pdf")
+boxplot(exprs(eset), outline=FALSE, horizontal = TRUE, main = "before quantile-normalisation")
+boxplot(exprs(neset), outline=FALSE, horizontal = TRUE, main = "after quantile-normalisation & log2(x+1)")
+dev.off()
+
+# TOO MUCH VARIATION IN THE MEAN INTENSITY AMONG CTR AND DMSO4 SAMPLES
+# CTR.2       CTR.3       CTR.4      CTR.3A
+# 7.4415708   0.5265584  14.7165392   0.5265584
+# REMOVE SAMPLES CTR.3, CTR.3A AND DMSO2
+
+print(colMeans(exprs(neset)))
+
+filterSample <- colnames(neset)[neset@phenoData@data$samples %nin% c("CTR.3", "CTR.3A", "DMSO2")]
+neset <- neset[, filterSample]
+
+# LIMMA DPE ------------------------------------------------------------------ #
+
+design <- model.matrix(~0 + groups, data = neset)
+colnames(design) <- gsub("groups", "", colnames(design))
+contrast =  makeContrasts(contrasts=c("CBZ.0.1-CTR","CBZ.100-CTR","CBZEP.0.1-CTR",
+                                      "CBZEP.100-CTR", "DMSO-CTR", 
+                                      "CBZ.0.1-DMSO",
+                                      "CBZ.100-DMSO", "CBZEP.0.1-DMSO",
+                                      "CBZEP.100-DMSO", "CTR-DMSO"), levels=design)
+fit1 <- lmFit(exprs(neset), design)
+fit2 <- contrasts.fit(fit1, contrasts = contrast)
+fit3 <- eBayes(fit2, trend = TRUE)
+
+lsContrast = list("CBZ.0.1-CTR","CBZ.100-CTR","CBZEP.0.1-CTR",
+                  "CBZEP.100-CTR", "DMSO-CTR", 
+                  "CBZ.0.1-DMSO",
+                  "CBZ.100-DMSO", "CBZEP.0.1-DMSO",
+                  "CBZEP.100-DMSO", "CTR-DMSO")
+names(lsContrast) <- c("CBZ.0.1-CTR","CBZ.100-CTR","CBZEP.0.1-CTR",
+                       "CBZEP.100-CTR", "DMSO-CTR", 
+                       "CBZ.0.1-DMSO",
+                       "CBZ.100-DMSO", "CBZEP.0.1-DMSO",
+                       "CBZEP.100-DMSO", "CTR-DMSO")
+lsRes = lapply(lsContrast, function(cf) topTable(fit3, 
+                                                 coef=cf, 
+                                                 n=Inf, 
+                                                 confint = TRUE, 
+                                                 sort.by="none")
+)
+lsResAnn = lapply(lsRes, function(X)
+  cbind.data.frame(
+    neset@featureData@data,
+    X
+  )
+)
+if(!file.exists("./RESULTS/limma_DEP_cbz.xlsx")) {
+  openxlsx::write.xlsx(lsResAnn, "./RESULTS/limma_DEP_cbz.xlsx", rowNames = TRUE)
+}
+# ---------------------------------------------------------------------------- #
+
+out <- list(dep = lsResAnn, eset = neset)
+
+return(out)
+}
+
+res <- f_clean_dep(
+  pathDat = "Samples View Report for Danio rerio 210826 all samples.xls",
+  impute = TRUE, method = "addZero"
+)
+# ---------------------------------------------------------------------------- #
+
+fdr.01 <-
+unlist(
+  lapply(res$dep, function(X)
+    nrow(
+      subset(
+    X, adj.P.Val < 0.1
+      )
+    )
+  )
+)
+###
+fdr.005 <-
+  unlist(
+    lapply(res$dep, function(X)
+      nrow(
+        subset(
+          X, adj.P.Val < 0.05
+        )
+      )
+    )
+  )
+###
+pval.005 <-
+  unlist(
+    lapply(res$dep, function(X)
+      nrow(
+        subset(
+          X, P.Value < 0.05
+        )
+      )
+    )
+)
+###
+
+thres = data.frame(
+  group = names(fdr.005),
+  FDR.005 = fdr.005,
+  FDR.01 = fdr.01,
+  PVAL.005 = pval.005,
+  row.names = NULL
+)
+# ---------------------------------------------------------------------------- #
